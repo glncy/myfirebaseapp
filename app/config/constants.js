c04e0af30df40170d685c70e43274ae55bb18fb3
Glency Tirao <glency.dagusoft@gmail.com>
2019-11-05 01:48:03 +0800

    Initial Commit

---
+++ app/config/constants.js
@@ -0,0 +1,7 @@
+export const FIREBASE_API_KEY = "AIzaSyC5H8Ae5ud8VYjFXNIIW_e8aTShE9IOX7E";
+export const FIREBASE_AUTH_DOMAIN = "myfirebaseapp-83106.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://myfirebaseapp-83106.firebaseio.com/";
+export const FIREBASE_PROJECT_ID = "myfirebaseapp-83106";
+export const FIREBASE_STORAGE_BUCKET = "gs://myfirebaseapp-83106.appspot.com/";
+export const FIREBASE_MESSAGING_SENDER_ID = "867195928179";
+export const FIREBASE_APP_ID = "1:867195928179:android:c01934212773435d257406";
